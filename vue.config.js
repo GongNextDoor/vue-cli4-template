fa7e6cb62b09805f34f452009ca3fdaea1a4f92d
GongNextDoor <1582016240@qq.com>
2021-04-20 16:41:37 +0800

    eslint规则修改

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,25 @@
 module.exports = {
-  publicPath: "./",
-  outputDir: "dist",
-  assetsDir: "static",
+  publicPath: './',
+  outputDir: 'dist',
+  assetsDir: 'static',
   productionSourceMap: false,
   devServer: {
     port: 300,
     open: true,
     overlay: {
       warnings: false,
-      errors: true,
+      errors: true
     },
     proxy: {
-      "/dev-api": {
-        target: "http://172.18.40.40:20001",
+      '/dev-api': {
+        target: 'http://172.18.40.40:20001',
         ws: true,
         changeOrigin: true,
-        logLevel: "debug",
+        logLevel: 'debug',
         pathRewrite: {
-          "^/dev-api": "",
-        },
-      },
-    },
-  },
-};
+          '^/dev-api': ''
+        }
+      }
+    }
+  }
+}
